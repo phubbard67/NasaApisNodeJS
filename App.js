eff71e229c88b299c7c036d8c4abf134bfa87eb0
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-08 01:12:02 -0800

    Finished getting all of the NEO Data and I output some data of interest! Now on to the next API

--- App.js
+++ App.js
@@ -20,6 +20,9 @@ const request = require('request');
 const fs = require('fs');
 const ini = require('ini');
 const CurrentDate = new Date();
+
+//---Astroid NeoWs Vars
+//Neo Feed Vars
 const DateInSevenDays = new Date();
 DateInSevenDays.setDate(CurrentDate.getDate() + 7);
 
@@ -35,16 +38,112 @@ strMonthInSevenDays = GetTwoDigitString(nMonthInSevenDays);
 
 const AstroidStartDate = `${CurrentDate.getFullYear()}-${strMonth}-${strDay}`;
 const AstroidEndDate = `${DateInSevenDays.getFullYear()}-${strSevenDays}-${strMonthInSevenDays}`;
-console.log(AstroidStartDate);
+
+//Neo Lookup Vars
+const AsteroidPKNine = '3542519'
 
 const options = {
-    latitude: 0,
-    longitude: 0,
     //-------------------------------------------------Astroid NeoWs
     //---Neo - Feed
-    ApiAstroidNeoFeed: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${AstroidStartDate}&end_date=${AstroidStartDate}&api_key=`
+    ApiAstroidNeoFeed: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${AstroidStartDate}&end_date=${AstroidStartDate}&api_key=`,
+    //---Neo - Lookup by Astroid ID
+    ApiAstriodLookup: `https://api.nasa.gov/neo/rest/v1/neo/${AsteroidPKNine}?api_key=`,
+    //---New - Browse all NEO data
+    ApiAsteroidBrowse: `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=`
+}  
+
+//-------------------------------------------Run All Astroid APIs
+const GetAsteroidNeoData = function(ApiKey)
+{
+    //======================Get the Asteriod Feed Information
+    const CurrentAsteriodFeed = request({url: `${options.ApiAstroidNeoFeed}${ApiKey}`, json: true, headers: options.headers}, function(error, response){
+        if(error){
+            console.log("Portland... We have problem");
+            console.log(error);
+        }
+        else{
+            console.log("========================Astroid / NEOs APIs>");
+            console.log("\n\n===NEOs Feed API>");
+            //Read out all Near Earth Ojects found
+            const nNumberOfNEOs = response.body.element_count;
+            console.log(`NEOs That are the closest to Earth in the next 7 days:`);
+            console.log(`   Number of NEOs in the Next 7 Days: ${nNumberOfNEOs}`);
+            const NearEarthObjects = response.body.near_earth_objects;
+            for(const date in NearEarthObjects)
+            {
+                if(response.body.hasOwnProperty.call(NearEarthObjects, date)){
+                    const NEOs = NearEarthObjects[date];
+                    console.log(`NEOs for Date: ${date}`);
+                    for(i = 0; i < nNumberOfNEOs; ++i)
+                    {
+                        console.log(`Astroid Name: ${NEOs[i].name}\nAstroid Potentially Hazardous: ${NEOs[i].is_potentially_hazardous_asteroid}`);
+                    }
+                }
+            }
+        }
+    });
+    //===End Astroid Feed
+
+    //======================Get A particular Asteroid
+    const CurrentCeresInformation = request({url: `${options.ApiAstriodLookup}${ApiKey}`, json: true, headers: options.headers}, function(error, response){
+        if(error){
+            console.log("Portland... We have problem");
+            console.log(error);
+        }
+        else{
+            //Break up the data
+            console.log("\n\n===NEOs Search API>");
+            console.log(`NEO API Hazard Result for Astriod: ${AsteroidPKNine}`)
+            console.log(`Astriod Name: ${response.body.name}\nPotentially Hazardous to Earth: ${response.body.is_potentially_hazardous_asteroid}`);
+        }
+    });
+    //===End Get A Particular Asteroid
+
+    //======================Browse all NEO Data
+    const CurrentBrowseInformation = request({url: `${options.ApiAsteroidBrowse}${ApiKey}`, json: true, headers: options.headers}, function(error, response){
+        if(error){
+            console.log("Portland... We have problem");
+            console.log(error);
+        }
+        else{
+            console.log("\n\n===NEOs Browse API>");
+            //Read out all Near Earth Ojects found
+            const nNumberOfNEOs = response.body.page.size;
+            console.log(`Astroids Returned from the Browse API:`);
+            console.log(`   Number of NEOs: ${nNumberOfNEOs}`);
+            const NearEarthObjects = response.body.near_earth_objects;
+            for(i = 0; i < nNumberOfNEOs; ++i)
+            {
+                const isHazardous = NearEarthObjects[i].is_potentially_hazardous_asteroid;
+                if(isHazardous == true)
+                {
+                    console.log(`\n------------------------------Astroid Name: ${NearEarthObjects[i].name}\n-POTENTIALLY HAZARDOUS TO EARTH!!!!!!!!!!!!`)
+                    const PotentialCloseCallDates = NearEarthObjects[i].close_approach_data;
+                    let dateBuff = PotentialCloseCallDates[0].close_approach_date;
+                    let approchYearBuffer = dateBuff.substring(0, 4);
+                    let intYearBuffer = parseInt(approchYearBuffer);
+
+                    j = 0;
+                    while(intYearBuffer < CurrentDate.getFullYear())
+                    {
+                        dateBuff = PotentialCloseCallDates[j].close_approach_date;
+                        approchYearBuffer = dateBuff.substring(0, 4);
+                        intYearBuffer = parseInt(approchYearBuffer);
+                        j++;
+                    }
+                    console.log(`---------------------The next potential colision to Earth will be: ${PotentialCloseCallDates[j].close_approach_date_full}\n`);
+                }
+                else
+                {
+                    console.log(`Astroid Name: ${NearEarthObjects[i].name}\nPotentially Hazardous to Earth: ${isHazardous}`)
+                }
+            }
+        }
+    });
+    //===End Browse all NEO Data
 }
  
+//--------------------Main App Functionality 
 try{
     //Get the API File data:
     // NOTE: If you want to create your own file, just 
@@ -60,17 +159,9 @@ try{
     //---End Comment our or remove...
 
     const ApiKey = ApiDatabase.NASA_API.API_KEY;
-    console.log(ApiKey);
+    
+    GetAsteroidNeoData(ApiKey);
 
-    const CurrentWeatherData = request({url: `${options.ApiAstroidNeoFeed}${ApiKey}`, json: true, headers: options.headers}, function(error, response){
-        if(error){
-            console.log("Portland... We have problem");
-            console.log(CurrentDate.getDate());
-        }
-        else{
-            console.log(response.body);
-        }
-    });
 
 }catch(error){
     console.log("Portland... We REALLY have a problem here.\n");
