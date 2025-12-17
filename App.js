6a8ec0ae2908330fb149ead36f8a625a04aa17c8
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-16 23:31:39 -0800

    Messing around with the server console output.

--- App.js
+++ App.js
@@ -95,10 +95,9 @@ try{
 //                 common.ErrorPrintFunc(error);
 //             }
 //             else{
-//                 console.log("YOU DID IT!!")
+//                 console.log("index.html has been served!")
 //             }
 //         });
-//         console.log("file sent")
 //     });
 
 //     app.listen(3000, (error) => {
@@ -107,7 +106,7 @@ try{
 //             common.ErrorPrintFunc(error);
 //         }
 //         else{
-//             console.log("The server is live at /http://localhost/3000/");
+//             console.log("The server is live at http://localhost:3000/");
 //         }
 //     });
 
